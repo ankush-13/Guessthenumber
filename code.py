8927884664c8dda1d13dd4d82161713cc1364b3c
ankush-13 <ankushpatle321@gmail.com>
2025-03-01 15:45:45 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,20 @@
+import random
 
+target = random.randint(1,100)
+
+while True:
+    userChoice = input("Guess the target or Quit: ")
+    if(userChoice == "Quit"):
+        break
+
+    userChoice = int(userChoice)
+    if(userChoice == target):
+        print("Success : Correct Guess!!")
+        break
+    elif(userChoice < target):
+        print("Your number is too low. Take a bigger number...")
+    else:
+        print("Your number is too big. Take a smaller number...")
+
+
+print("_____GAME OVER______")
